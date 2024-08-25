0badd3068eb66731a365985669a992d87ff802d1
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-08-25 11:57:27 +0300

    add paymentSummaryFile

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,4 @@
-/import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 // import './data/cart-class.js';
--- js/checkout/paymentSummary.js
+++ js/checkout/paymentSummary.js
@@ -1,26 +1,105 @@
-import { cartItems } from "../EntitiesData/cartData";
-import { getProductByID } from './product.js';
-import {getDeliveryOptions} from '../EntitiesData/deliveryOptions.js'
+import { cart } from "../entities/cart.js";
+import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../data/order.js";
 
-export function calculateTotalProductPriceCents(){
-  const totalPayments=0;
-  cartItems.forEach(element => {
-    const product =getProductByID(element.id);
-    totalPayments+=product.priceCents * element.quantity;
-    console.log(totalPayments);
-  });
-  return totalPayments;
+function renderPaymentSummaryHTML() {
+  const totalProductPriceCents = cart.calculateTotalProductPriceCents();
+  const totalShippingPriceCents = cart.calculateTotalShippingPriceCents();
+  const totalBeforeTaxCents = totalProductPriceCents + totalShippingPriceCents;
+  const estimatedTaxCents = Math.round(totalBeforeTaxCents * 0.1);
+  const orderTotalCents = totalBeforeTaxCents + estimatedTaxCents;
+
+  const html = `
+    <div class="payment-summary-title">
+      Order Summary
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Items (${cart.calculateCartQuantity()}):</div>
+      <div class="payment-summary-money">$${formatCurrency(
+        totalProductPriceCents
+      )}</div>
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Shipping &amp; handling:</div>
+      <div class="payment-summary-money js-total-shipping-price-cents">$${formatCurrency(
+        totalShippingPriceCents
+      )}</div>
+    </div>
+
+    <div class="payment-summary-row subtotal-row">
+      <div>Total before tax:</div>
+      <div class="payment-summary-money">$${formatCurrency(
+        totalBeforeTaxCents
+      )}</div>
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Estimated tax (10%):</div>
+      <div class="payment-summary-money">$${formatCurrency(
+        estimatedTaxCents
+      )}</div>
+    </div>
+
+    <div class="payment-summary-row total-row">
+      <div>Order total:</div>
+      <div class="payment-summary-money js-order-total-price-cent">$${formatCurrency(
+        orderTotalCents
+      )}</div>
+    </div>
+
+    <button class="place-order-button js-place-order button-primary">
+      Place your order
+    </button>`;
+
+  return html;
 }
 
-export function renderPaymentSummary(){
-  let productPriceCents =calculateTotalProductPriceCents();
-  let shippingPriceCents=0;
+function displayPaymentSummary() {
+  const paymentSummaryElement = document.querySelector(".js-payment-summary");
 
-  cartItems.forEach(element => {
-    shippingPriceCents +=getDeliveryOptions(element.deliveryOptionId).priceCents;
+  if (!paymentSummaryElement) {
+    return;
+  }
 
-    
-  });
-  console.log(shippingPriceCents);
+  paymentSummaryElement.innerHTML = renderPaymentSummaryHTML(cart);
+}
 
+function addClickEventForPlaceOrder() {
+  document
+    .querySelector(".js-place-order")
+    .addEventListener("click", async () => {
+      try {
+        const response = await fetch("https://supersimplebackend.dev/orders", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            cart: cart,
+          }),
+        });
+
+        const order = await response.json();
+        addOrder(order);
+      } catch (error) {
+        console.log("Unexpected error. Try again later.");
+      }
+
+      openOrderPage();
+    });
+}
+
+function addEventHandlers() {
+  addClickEventForPlaceOrder();
+}
+
+function openOrderPage() {
+  window.location.href = "orders.html";
+}
+
+export function renderPaymentSummary() {
+  displayPaymentSummary();
+  addEventHandlers();
 }
